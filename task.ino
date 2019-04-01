1272f31cedce6396a0727c6f233bd0bb54c985e3
Syzygianinfern0 <spsharan2000@gmail.com>
2019-04-01 11:40:10 +0530

    ADC conversion with accelerometer

--- task.ino
+++ task.ino
@@ -1,13 +1,92 @@
-#include<avr/io.h>
-#include<util/delay.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#define X 0
+#define Y 0
+#define Z 0
+volatile int k = 0;
+volatile int last_x, last_y, last_z;
+ISR(TIMER0_OVF_vect)
+{
+  ++k;
+}
+
+void dlay(float t)
+{
+  TCCR0B = (1 << CS02) | (1 << CS00);
+  k = 0;
+  while (k <= 10)
+    ;
+  TCCR0B = 0;
+}
+void init()
+{
+  TIMSK0 = 1 << TOIE0;
+  ADMUX = (1 << REFS0); /* Vref: Avcc, ADC channel: 0 */
+  ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+}
+
+/*
+  ADC Read function
+*/
+uint16_t ADC_Read(unsigned char channel)
+{
+  ADMUX = (1 << REFS0) | (channel & 0x07); /* set input channel to read */
+  ADCSRA |= (1 << ADSC);                   /* Start ADC conversion */
+  while (!(ADCSRA & (1 << ADIF)))
+    ;                    /* Wait until end of conversion by polling ADC interrupt flag */
+  ADCSRA |= (1 << ADIF); /* Clear interrupt flag */
+  return ADC;            /* Return ADC word */
+}
 int main()
 {
-  DDRB=0b00100000;
-  PORTB=0b00000000;
-  while(1)
+  init();
+  sei();
+  int x, y, z;
+  Serial.begin(9600);
+  while (1)
   {
-    PORTB ^=1<<PORTB5; //^-EXOR
-    _delay_ms(1000);   
+    x = y = z = 0;
+
+    x = ADC_Read(0);
+    y = ADC_Read(1);
+    z = ADC_Read(2);
+
+    Serial.print("x= ");
+    Serial.print(x);
+    Serial.print("  y= ");
+    Serial.print(y);
+    Serial.print("  z= ");
+    Serial.println(z);
+    dlay(0.01);
+    if (x >= 265 && x <= 295 && y <= 360 && y >= 330 && z <= 365 && z >= 330)
+    {
+      Serial.println("IDLE");
+    }
+    else if (x >= 295 && x <= 330 && y <= 420 && y >= 379 && z <= 366 && z >= 330)
+    {
+      Serial.println("FWD");
+    }
+    else if (x >= 295 && x <= 330 && y <= 295 && y >= 260 && z <= 366 && z >= 335)
+    {
+      Serial.print("BACK");
+    }
+    else if (x >= 295 && x <= 330 && y >= 335 && y <= 367 && z >= 280 && z <= 315)
+    {
+      Serial.print("LEFT");
+    }
+    else if (x <= 315 && x >= 280 && y >= 335 && y <= 367 && z <= 425 && z >= 380)
+    {
+      Serial.print("RIGHT");
+    }
+    /*
+    else if (z>=216 && z<=252 && y>=210 && y<=229) {
+      Serial.print(5);
+    }
+    else if (z>=216 && z<=252 && y<210) {
+      Serial.print(6);
+    }
+    else if (z>=216 && z<=252 && y>250) {
+      Serial.print(7);
+    }*/
   }
-  return 0;  
 }
