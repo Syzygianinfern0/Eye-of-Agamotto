7c90d903b183e4d305bbba35c28feb52ef5a6366
Syzygianinfern0 <spsharan2000@gmail.com>
2019-04-01 12:19:07 +0530

    Serial to USART

---
+++ Comms.py
@@ -0,0 +1,13 @@
+from time import sleep
+import serial
+# Establish the connection on a specific port
+ser = serial.Serial('/dev/tty.usbmodem1d11', 9600)
+counter = 32  # Below 32 everything in ASCII is gibberish
+while True:
+    counter += 1
+    # Convert the decimal number to ASCII then send it to the Arduino
+    ser.write(str(chr(counter)))
+    print(ser.readline())  # Read the newest output from the Arduino
+    sleep(.1)  # Delay for one tenth of a second
+    if counter == 255:
+        counter = 32
--- task.ino
+++ task.ino
@@ -1,34 +1,45 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#define X 0
-#define Y 0
-#define Z 0
+#include <avr/iom328p.h>
+
 volatile int k = 0;
-volatile int last_x, last_y, last_z;
 ISR(TIMER0_OVF_vect)
 {
   ++k;
+}
+void usart_write(String data)
+{
+  for (int i = 0; data[i] != '\0'; i++)
+  {
+    while (!(UCSR0A & (1 << UDRE0)))
+      ;
+    UDR0 = data[i];
+  }
 }
 
 void dlay(float t)
 {
   TCCR0B = (1 << CS02) | (1 << CS00);
   k = 0;
-  while (k <= 10)       // Some delay thing
+  while (k <= 10)
     ;
   TCCR0B = 0;
 }
-void init()             // Init fir timers and ADC
+void init()
 {
   TIMSK0 = 1 << TOIE0;
   ADMUX = (1 << REFS0); /* Vref: Avcc, ADC channel: 0 */
   ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+  UCSR0A = 0X00;
+  UCSR0B = (1 << RXEN0) | (1 << TXEN0);
+  UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+  UBRR0 = 103;
 }
 
 /*
   ADC Read function
 */
-uint16_t ADC_Read(unsigned char channel)   // Reads from the channel passed. Anhy analog pin
+uint16_t ADC_Read(unsigned char channel)
 {
   ADMUX = (1 << REFS0) | (channel & 0x07); /* set input channel to read */
   ADCSRA |= (1 << ADSC);                   /* Start ADC conversion */
@@ -42,7 +53,6 @@ int main()
   init();
   sei();
   int x, y, z;
-  Serial.begin(9600);
   while (1)
   {
     x = y = z = 0;
@@ -50,33 +60,32 @@ int main()
     x = ADC_Read(0);
     y = ADC_Read(1);
     z = ADC_Read(2);
-
-    Serial.print("x= ");
-    Serial.print(x);
-    Serial.print("  y= ");
-    Serial.print(y);
-    Serial.print("  z= ");
-    Serial.println(z);
+    usart_write("\nx= ");
+    usart_write((String)x);
+    usart_write("\ty= ");
+    usart_write((String)y);
+    usart_write("\tz= ");
+    usart_write((String)z);
     dlay(0.01);
     if (x >= 265 && x <= 295 && y <= 360 && y >= 330 && z <= 365 && z >= 330)
     {
-      Serial.println("IDLE");
+      usart_write("\nIDLE");
     }
     else if (x >= 295 && x <= 330 && y <= 420 && y >= 379 && z <= 366 && z >= 330)
     {
-      Serial.println("FWD");
+      usart_write("\nFWD");
     }
     else if (x >= 295 && x <= 330 && y <= 295 && y >= 260 && z <= 366 && z >= 335)
     {
-      Serial.print("BACK");
+      usart_write("\nBACK");
     }
     else if (x >= 295 && x <= 330 && y >= 335 && y <= 367 && z >= 280 && z <= 315)
     {
-      Serial.print("LEFT");
+      usart_write("\nLEFT");
     }
     else if (x <= 315 && x >= 280 && y >= 335 && y <= 367 && z <= 425 && z >= 380)
     {
-      Serial.print("RIGHT");
+      usart_write("\nRIGHT");
     }
   }
 }
